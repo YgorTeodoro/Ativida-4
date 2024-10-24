1d3c18098fb359f3fc26ee981847609d04048385
YgorTeodoro <grupoandrade80@gmail.com>
2024-10-23 21:57:44 -0300

    Add files via upload

---
+++ atividadeigor.py
@@ -0,0 +1,14 @@
+# Exercicio 1
+nome = input('nome ')
+sobrenome = input('sobrenome ')
+idade = input('idade  ')
+print (nome, sobrenome, idade)
+
+# Exercicio 2
+
+listaA = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+
+abc = int(input('Informe o mês: '))
+calc = abc-1
+
+print(listaA[calc])
